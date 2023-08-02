46249b85ba769bed4c81638b7ec2c26cf5d24ad0
Milos Ilic <milosilic89@outlook.com>
2023-08-02 19:38:57 +0200

    Added FriendsList and Button Components

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ export default function App() {
     <div className='app'>
       <div className='sidebar'>
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
     </div>
   );
@@ -59,7 +61,25 @@ function Friend({ friend }) {
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-      <button className='button'>Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className='button'>{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className='form-add-friend'>
+      <label>👯 Friend Name</label>
+      <input type='text' />
+
+      <label>🖼️ Image URL</label>
+      <input type='text' />
+
+      <Button>Add</Button>
+    </form>
   );
 }
